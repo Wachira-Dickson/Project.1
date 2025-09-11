5d13a55b2a10fa0a90b984b79ac245979d7a2bc3
Your Name <dicksonwachira886@gmail.com>
2025-09-11 17:28:26 +0300

    Create a basic model for a listing

---
+++ app/Models/Listing.php
@@ -0,0 +1,27 @@
+<?php
+    namespace App\Models;
+
+    class Listing {
+        public static function all() {
+            return [
+                [ 'id' => 1,
+                  'title' => 'Listing One',
+                  'description' => 'This is the description for listing one'
+                ],
+                [ 'id' => 2,
+                  'title' => 'Listing Two',
+                  'description' => 'This is the description for listing two'
+                ]
+            ];
+        }
+        public static function find($id) {
+            $listings = self::all();
+
+            foreach($listings as $listing) {
+                if($listing['id'] == $id) {
+                    return $listing;
+                }
+            }
+            return null;
+        }
+    }
